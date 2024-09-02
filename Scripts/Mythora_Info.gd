917251e5ff5b8dc361cc5d17ce77077c3c3a0544
Nicholas Taveras <taveras123n@gmail.com>
2024-09-02 14:07:29 -0400

    Renamed Mythora_Res to Mythora_Info

---
+++ Scripts/Mythora_Info.gd
@@ -0,0 +1,16 @@
+extends Resource
+class_name Mythora_Info
+
+@export var display_name : String
+@export var nature : Nature
+@export var move_set_dict : Dictionary
+@export var starting_combat_actions : Array[CombatAction]
+@export var visual : Texture2D
+
+@export_category("BaseStats")
+@export var hp : int
+@export var speed : int
+@export var armor : int
+@export var magic_resist : int
+@export var attack_damage : int
+@export var ability_power : int
